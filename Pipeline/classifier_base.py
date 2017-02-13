cfeade2c42d20ea023d496aba58611091f5a3faf
lgoerke <lgoerke@uni-osnabrueck.de>
2017-02-13 12:14:37 +0100

    added clsfr base class

---
+++ Pipeline/classifier_base.py
@@ -0,0 +1,14 @@
+class classifier_base:
+	'''Classifier base class
+
+	Implements functions init, fit and predict
+
+	'''
+    def __init__(self,params):
+    	pass
+
+    def fit(train_imgs,train_labels):
+    	pass
+
+    def predict(test_imgs):
+    	pass

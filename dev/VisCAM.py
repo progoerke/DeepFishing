bd76384f2f319cd8bca27255650e3d32277d88b8
Germonda <germondareijnen@gmail.com>
2017-02-23 10:12:00 +0100

    VisCAM with VGG16

--- dev/VisCAM.py
+++ dev/VisCAM.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import os
-import sys
 
-sys.path.append('/usr/local/lib/python2.7/site-packages')
+from keras.preprocessing import image
+from imagenet_utils import preprocess_input
+from keras.models import Model
 
 from keras.preprocessing.image import img_to_array
 from vis.utils import utils
@@ -11,27 +12,35 @@ from vis.utils.vggnet import VGG16
 from keras.applications.vgg19 import VGG19
 from keras.applications.resnet50 import ResNet50
 from keras.applications.inception_v3 import InceptionV3
+from keras.applications.xception import Xception
+from keras.utils.np_utils import to_categorical
 from vis.visualization import visualize_cam
+#from visualization import visualize_cam
 from skimage import io
 
 from VisCAM_NNparams import synset_to_dfs_ids
 #from VisCAM_NNparams import detect_configuration
 
 # Build the VGG19 network with ImageNet weights
-model = VGG19(weights='imagenet', include_top=True)
+model = VGG16(weights='imagenet', include_top=True)
+#model = VGG19(weights='imagenet', include_top=True)
+#model = ResNet50(weights='imagenet', include_top=True)
 #model = InceptionV3(weights='imagenet', include_top=True)
+#model = Xception(weights='imagenet', include_top=True)
+
 print('Model loaded.')
 
 # The name of the layer we want to visualize
 # (see model definition in vggnet.py)
-layer_name = 'predictions'
+layer_name = 'predictions' # VGG16, VGG19, InceptionV3
+#layer_name = 'fc1000' # ResNet50
 layer_idx = [idx for idx, layer in enumerate(model.layers) if layer.name == layer_name][0]
 
 #model_type, index = detect_configuration(model)
 #s = "n02512053"  # Imagenet code for "fish"
 #s = "n02545841"  # Imagenet code for "Opah fish"
 #s = "n02626762"  # Imagenet code for "tuna"
-s = "n02536864"  # Imagenet code for "salmon" = 391
+s = "n02536864"  # Imagenet code for "silver salmon" = 391
 pred_class = synset_to_dfs_ids(s)
 
 def get_file_list(path,ext='',queue=''):
@@ -49,8 +58,12 @@ prob_imgs=[]
 heatmaps=[]
 for path in image_paths:
     # Predict the corresponding class for use in `visualize_saliency`.
-    seed_img = utils.load_img(path, target_size=(224, 224))
-    seed_array=np.array([img_to_array(seed_img)])
+    seed_img = utils.load_img(path, target_size=(224, 224)) # VGG16, VGG19, ResNet50
+    #seed_img = utils.load_img(path, target_size=(299, 299)) # Inception_V3, Xception
+    #x = image.img_to_array(seed_img)
+    #x = np.expand_dims(x, axis=0)
+    #seed_array = preprocess_input(x)
+    seed_array = np.array([img_to_array(seed_img)])
     probs=model.predict(seed_array)
     prob_imgs.append(probs[0,pred_class])
 

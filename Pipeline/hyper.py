76fd631a2060560dd78898b9af88b1591a83d4d9
kstandvoss <kstandvoss@uos.de>
2017-03-09 12:23:10 +0100

    right indices for crossvalidation in hyper

--- Pipeline/hyper.py
+++ Pipeline/hyper.py
@@ -12,7 +12,7 @@ sys.path.insert(0,parentdir)
 from hyperopt import Trials, STATUS_OK, tpe
 from hyperopt import hp, fmin, tpe
 
-#from code.data_harvesting import heatmap_VisCAM
+from code.data_harvesting import heatmap_VisCAM
 from code.data_harvesting import dataloader
 from code.CV import VGG_CV as CV
 
@@ -29,17 +29,18 @@ def data():
     
     cval_splits = 5
 
-    data, labels, _, _ = dataloader.load_train(use_chached=True)
-    cval_indx = CV.VGG_CV(data[:20], labels[:20], folds=cval_splits)
+    data, labels, _, _ = dataloader.load_train(use_chached=True), use_heatmap=True)
 
-    print(cval_indx)
-    train_indx = cval_indx[:-1]
+    cval_indx = CV.VGG_CV(data, labels, folds=cval_splits)
+    indx = [np.where(cval_indx==ind) for ind in np.unique(cval_indx)]
+
+    train_indx = np.hstack(indx[:-1])
     X_train = data[train_indx]
-    Y_train = label[train_indx]
+    Y_train = labels[train_indx]
 
-    val_indx = cval_indx[-1]
+    val_indx = indx[-1]
     X_val = data[val_indx]
-    Y_val = data[val_indx]
+    Y_val = labels[val_indx]
 
     return X_train, Y_train, X_val, Y_val
 
@@ -51,7 +52,7 @@ Wrapper function to run model training and evaluation
 '''
 def run_model(params):  
 
-    classifier = CCN(*params)
+    classifier = CCN(size = (32, 32), nb_classes = 8, nb_epoch = 12, *params)
 
     classifier.fit(X_train, Y_train, X_val, Y_val)
     loss = classifier.evaluate(X_val, Y_val)
@@ -72,7 +73,7 @@ if __name__ == '__main__':
 
     space = CCN.space
 
-    best_run = fmin(model, space, algo = tpe.suggest, max_evals = max_evals)
+    best_run = fmin(run_model, space, algo = tpe.suggest, max_evals = max_evals)
 
     print(best_run)
     pickle.dumb(best_run, open('optResults.pkl', 'wb'))

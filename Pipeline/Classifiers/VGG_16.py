1b67bb54df4be8243cdd33f037b6f4baaa2429e4
Sebastian Tiesmeyer <sebastian.tiesmeyer@gmail.com>
2017-03-31 18:47:14 +0200

    Added peak detection routine for a patch matrix

---
+++ Pipeline/Classifiers/VGG_16.py
@@ -0,0 +1,164 @@
+### credit rdcolema
+
+import numpy as np
+
+from keras.layers.normalization import BatchNormalization
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers import ZeroPadding2D, MaxPooling2D, GlobalAveragePooling2D, Convolution2D, AveragePooling2D
+from keras.layers import Input, Activation, Lambda
+from keras.models import Sequential, Model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.resnet50 import identity_block, conv_block
+from keras.utils.layer_utils import convert_all_kernels_in_model
+from keras.callbacks import EarlyStopping
+from keras import optimizers
+
+
+from Classifiers.classifier_base import Classifier_base
+
+from math import log
+
+from hyperopt import hp
+###################################### ----- VGG_16 MODEL ----- #################################################
+
+def VGG_preprocess(x):
+    x /= 255.
+    x -= 0.5
+    x *= 2.
+    return x
+
+
+class VGG_16(Classifier_base):
+
+
+    space = (
+        hp.loguniform('lr', log(1e-4), log(1)),
+        hp.choice('batch_size',[8,16,32,64]),
+        hp.choice('optimizer',['adam','adadelta','sgd'])
+    )
+    """
+    The VGG16 Imagenet model with Batch Normalization for the Dense Layers
+    """
+    def __init__(self, size=(224, 224), n_classes=2, nb_epoch = 12, lr=0.001, batch_size=64, optimizer='adam'):
+        self.size = size
+        self.n_classes = n_classes
+        self.nb_epoch = nb_epoch
+        self.lr = lr
+        self.optimizer = optimizer
+        self.batch_size = batch_size
+
+        self.class_weight = None
+        self.model = self.build()
+
+    def build(self):
+        """
+        Loads preconstructed VGG model from keras without top classification layer;
+        Stacks custom classification layer on top;
+        Returns stacked model
+        """
+        #img_input = Input(shape=(self.size[0], self.size[1], 3))
+        VGG = VGG16(include_top=False, weights='imagenet', input_shape=(self.size[0], self.size[1], 3))
+
+        for layer in VGG.layers:
+            layer.trainable = False
+
+        output = VGG.output
+        output = AveragePooling2D((8, 8), strides=(8, 8), name='avg_pool')(output)
+        output = Flatten(name='flatten')(output)
+        output = Dense(4096, activation='relu')(output)
+        output = Dropout(0.5)(output)
+        output = Dense(self.n_classes, activation='softmax', name='predictions')(output)
+
+        model = self.model = Model(VGG.input, output)
+
+        if self.optimizer == 'adam':
+            opt = optimizers.adam(lr=self.lr)
+        elif self.optimizer == 'adadelta':
+            opt = optimizers.adadelta(lr = self.lr)
+        else:
+            opt=optimizers.SGD(lr=self.lr, momentum=0.9, decay=0.0, nesterov=True)
+        
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=opt,
+                      metrics=["accuracy"])
+        return model
+
+    def get_datagen(self, aug=False):
+        if aug:
+            return ImageDataGenerator(rotation_range=10, width_shift_range=0.05, zoom_range=0.05,
+                                      channel_shift_range=10, height_shift_range=0.05, shear_range=0.05,
+                                      horizontal_flip=True, preprocessing_function=VGG_preprocess)
+        return ImageDataGenerator(preprocessing_function=VGG_preprocess)
+
+    def fit_val(self, trn_path, val_path, nb_trn_samples, nb_val_samples, nb_epoch=1, callbacks=[], aug=False):
+        """
+        Custom fit method for training with validation data and option for data augmentation"
+        """
+        train_datagen = self.get_datagen(aug=aug)
+        val_datagen = self.get_datagen(aug=False)
+
+        trn_gen = train_datagen.flow_from_directory(trn_path, target_size=self.size, batch_size=self.batch_size,
+                                                    class_mode='categorical', shuffle=True)
+        val_gen = val_datagen.flow_from_directory(val_path, target_size=self.size, batch_size=self.batch_size,
+                                                  class_mode='categorical', shuffle=True)
+        self.model.fit_generator(trn_gen, samples_per_epoch=nb_trn_samples, nb_epoch=nb_epoch, verbose=2,
+                                 validation_data=val_gen, nb_val_samples=nb_val_samples, callbacks=callbacks)
+
+
+
+    def fit_full(self, trn_path, nb_trn_samples, nb_epoch=1, callbacks=[], aug=False):
+        """
+        Custom fit method for training without validation data and option for data augmentation
+        """
+        train_datagen = self.get_datagen(aug=aug)
+
+        trn_gen = train_datagen.flow_from_directory(trn_path, target_size=self.size, batch_size=self.batch_size,
+                                                    class_mode='categorical', shuffle=True)
+        self.model.fit_generator(trn_gen, samples_per_epoch=nb_trn_samples, nb_epoch=nb_epoch, verbose=2,
+                                 callbacks=callbacks)
+
+
+    def test(self, test_path, nb_test_samples, aug=False):
+        """
+        Custom prediction method with option for data augmentation
+        """
+        test_datagen = self.get_datagen(aug=aug)
+        test_gen = test_datagen.flow_from_directory(test_path, target_size=self.size, batch_size=self.batch_size,
+                                                    class_mode=None, shuffle=False)
+        return self.model.predict_generator(test_gen, val_samples=nb_test_samples), test_gen.filenames
+
+    '''
+    Create weights for inbalanced classes
+    @param labels_dict: number of samples per label
+    @param mu: weighting
+    '''
+    def create_class_weight(self, labels_dict, mu=0.15):
+        values = np.fromiter(iter(labels_dict.values()), dtype=float)
+        total = np.sum(values)
+        keys = labels_dict.keys()
+        self.class_weight = dict()
+
+        print(values)
+        for key in keys:
+            score = log(mu*total/float(values[key]))
+            self.class_weight[key] = score if score > 1.0 else 1.0
+
+
+    def fit(self, train_generator, validation_generator, split):
+
+        early = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='auto')
+
+        if not self.class_weight:
+            self.class_weight = 'auto'
+
+        self.model.fit_generator(train_generator, steps_per_epoch=split[0]//self.batch_size, validation_data=validation_generator, validation_steps=split[1]//self.batch_size, callbacks=[early], epochs = self.nb_epoch, verbose = 1, class_weight = self.class_weight)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test, batch_size = self.batch_size, verbose = 1)
+
+    def evaluate(self, validation_generator, split):
+        
+        score = self.model.evaluate_generator(validation_generator, steps=split//self.batch_size)    
+        return score[0] 
--- dev_remainder/peak_finder.py
+++ dev_remainder/peak_finder.py
@@ -3,69 +3,104 @@ import numpy as np
 from scipy.signal import medfilt, convolve2d
 from scipy.ndimage.filters import gaussian_filter
 import scipy.stats as st
+import cv2
 
 
-kernlen=7 
-nsig=1
-interval = (2*nsig+1.)/(kernlen)
-x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
-kern1d = np.diff(st.norm.cdf(x))
-kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
-kernel = kernel_raw/kernel_raw.sum()
-
-
-edg =3
-d = np.random.random((30,30))
-thresh = .5
-res = 1
-
-if d.any: #for the case of non zero raw image
+def find_peaks(img, thresh = .5, edg = 3,kernel = 7):
+    '''
+    Finds peak location in a 2d density.
     
-    d = medfilt(d,[3,3]);
+    @args:
+    -img: [x,y] density
     
-    # apply threshold
-    if (d.dtype == 'uint8'):
-        d=d*np.array(d>thresh).astype(np.uint8)
-    else:
-        d=d*np.array(d>thresh).astype(np.uint16)
-
+    @opt args:
+    -thresh: Threshold value (-> fight noise)
+    -edg: Edge length of the peak search kernel
+    -kernel: Size of a gaussian kernel that blurs the density (-> fight noise)
     
-    if d.any: #for the case of the image is still non zero
+    @output:
+    -cent: [[x,x,x],[y,y,y]] values of all the peaks found in the image 
+    -cent_map: peak topography on a boolean matrix of [img.shape]
+    '''
+    kernlen=kernel
+    nsig=1
+    interval = (2*nsig+1.)/(kernlen)
+    x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
+    kern1d = np.diff(st.norm.cdf(x))
+    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
+    kernel = kernel_raw/kernel_raw.sum()
+    
+    if img.any: #for the case of non zero raw image
         
-        # smooth image
-        d=convolve2d(d.astype(np.float32),kernel,'same') 
+        img = medfilt(img,[3,3]);
         
-        # Apply again threshold (and change if needed according to SNR)
-        d=d*(d>0.9*thresh)
+        # apply threshold
+        if (img.dtype == 'uint8'):
+            img=img*np.array(img>thresh).astype(np.uint8)
+        else:
+            img=img*np.array(img>thresh).astype(np.uint16)
+    
         
-        if res: # switch between local maxima and sub-pixel methods
+        if img.any: #for the case of the image is still non zero
             
-            #case 1 # peak find - using the local maxima approach - 1 pixel resolution
+            # smooth image
+            img=convolve2d(img.astype(np.float32),kernel,'same') 
+            
+            # Apply again threshold (and change if needed according to SNR)
+            #print(img)
+            img=img*(img>0.9*thresh)
+            print(img)
+            print(0.9 * thresh)
+
                 
-                # d will be noisy on the edges, and also local maxima looks
-                # for nearest neighbors so edge must be at least 1. We'll skip 'edge' pixels.
-            sd=d.shape
-            x, y=np.where(d[edg:sd[0]-edg,edg:sd[1]-edg]);
-            
+                #peak find - using the local maxima approach - 1 pixel resolution   
+                    # d will be noisy on the edges, and also local maxima looks
+                    # for nearest neighbors so edge must be at least 1. We'll skip 'edge' pixels.
+            sd=img.shape
+            [x, y]=np.where(img[edg:sd[0]-edg,edg:sd[1]-edg]);
+            print(x,y)
             # initialize outputs
             cent=[];#
             cent_map=np.zeros((sd));
             
-            x=x+edg-1;
-            y=y+edg-1;
+            x=x+edg
+            y=y+edg
             for j in range(0,len(y)-1):
-                if  (d[x[j],y[j]] == np.max(np.max(d[x[j]-1:x[j]+1,y[j]-1:y[j]+1]))):
-
-                        
-                    cent.append([y[j] , x[j]]);
-                    cent_map[x[j],y[j]]=cent_map[x[j],y[j]]+1; # if a binary matrix output is desired
-
-        
-    else: # in case image after threshold is all zeros
-        cent=[];
-        cent_map=zeros(size(d));
-
-
+                if  (img[x[j],y[j]] == np.max(np.max(img[x[j]-1:x[j]+2,y[j]-1:y[j]+2]))):
+                        cent.append([y[j] , x[j]]);                        
+                        cent_map[x[j],y[j]]=cent_map[x[j],y[j]]+1; # if a binary matrix output is desired
     
-else: # in case raw image is all zeros (dead event)
-    cent=[];
+            
+        else: # in case image after threshold is all zeros
+            cent=[];
+            cent_map=zeros(size(img));
+    
+    
+        
+    else: # in case raw image is all zeros (dead event)
+        cent=[];
+    
+    return cent, cent_map 
+
+def fish_locator_3000(map,x=400,y=400):    
+    '''
+    Peak location applied to our task.
+    
+    @args:
+    -map: [x,y] p values of the patches for one single img (-> conserve topographical location).  
+    
+    @opt args:
+    -x: width of the original image (the one the map was obtained from)
+    -y: height of the original image 
+    
+    @output:
+    -map1: [[x,x,x],[y,y,y]] values of all the peaks found in the image 
+    -cent_map: peak topography on a boolean matrix of [img.shape]
+    ''' 
+    enlarged = cv2.resize(np.divide(map,np.max(map)),(400,400))
+    map = np.divide(map,np.max(map))
+    
+    map1,cmap = find_peaks(enlarged,.4,10,10)
+    
+    return map1,cmap
+

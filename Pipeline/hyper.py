0041c0f397f71f9fca36b7ad59eb16f820979d6e
kstandvoss <kstandvoss@uos.de>
2017-03-31 13:55:47 +0200

    spelling error

--- Pipeline/hyper.py
+++ Pipeline/hyper.py
@@ -210,7 +210,7 @@ if __name__ == '__main__':
                       metrics=["accuracy"])
         run_model(m=model,data=data, labels=labels,train_indx=train_indx,val_indx=val_indx)
     
-    test, filenames, _ = dataloader.load_test(filepath='/work/kstandvoss/test_mat.hdf5', use_chached=True, use_heatmap=False)
+    test, filenames, _ = dataloader.load_test(filepath='/work/kstandvoss/test_mat.hdf5', use_cached=True, use_heatmap=False)
     print(filenames) 
     preds = model.predict(test)
     write_submission('first',preds, filenames)

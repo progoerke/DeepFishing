b536ebfcc1197f19c47b0a88b2e0c25e7d44ee61
lgoerke <gdemo008@gcn1.cartesius.surfsara.nl>
2017-04-06 15:37:41 +0200

    pre network training

--- Pipeline/pre_network.py
+++ Pipeline/pre_network.py
@@ -44,7 +44,6 @@ class Pre_Network:
         for layer in base_model.layers:
           layer.trainable = False
 
-
         x = base_model.output
         x = Convolution2D(1024, 3,3, activation='relu')(x)
         predictions = Convolution2D(2, 1, 1, activation='relu')(x)
@@ -65,109 +64,6 @@ class Pre_Network:
         sys.stdout.write('\n Doooone :)\n')
         return images, ids
 
-    def shift_and_stitch(self,im, patch_size, stride):
-        ''' Return a full resolution segmentation by segmenting shifted versions
-        of the input 'im' and stitching those segmentations back together. 
-        The stride determines how many times the image should be shifted in the x and y direction'''
-            
-        ## create the output shape, the output image is made a bit bigger to allow the stitched versions to be added. 
-        ## the extra parts will be cut off at the end
-        output = np.zeros([im.shape[0]+97, im.shape[0]+97])
-        # the input image has to be padded with zeros to allow shifting of the image. 
-        # pad input image (half filter size + stride)
-        #im_padded = np.pad(im, ((patch_size//2, patch_size + stride),
-        #                        (patch_size//2, patch_size + stride)), 'constant', constant_values = [0,0])    
-        
-        im_padded = np.zeros((im.shape[0]+97,im.shape[1]+97,im.shape[2]))
-
-        im_padded[:,:,0] = np.pad(im[:,:,0], ((patch_size//2, patch_size + stride),
-            (patch_size//2, patch_size + stride)), 'constant', constant_values = [0,0])
-        im_padded[:,:,1] = np.pad(im[:,:,1], ((patch_size//2,patch_size + stride),
-            (patch_size//2, patch_size + stride)), 'constant', constant_values = [0,0])
-        im_padded[:,:,2] = np.pad(im[:,:,2], ((patch_size//2, patch_size + stride),
-            (patch_size//2, patch_size + stride)), 'constant', constant_values = [0,0])
-
-        im_p_sh = im_padded.shape
-        
-        # - obtain a shifted version of the image
-        # - apply the fully convolutional network
-        # - place the network output in the output of this function        
-        for row in range(0, stride):
-            for col in range(0, stride):
-                sys.stdout.write(".")
-                sys.stdout.flush()   
-                        
-                shifted_im = np.roll(im_padded, -row, axis=0)            
-                shifted_im = np.roll(shifted_im, -col, axis=1)
-                
-                # forward pass
-                probability = self.get99(shifted_im)
-                for i in range(probability.shape[0]):
-                    for j in range(probability.shape[1]):
-                        output[row+i*stride,col+j*stride] = probability[i,j,1]
-        
-        return output[0:im.shape[0], 0:im.shape[1]]
-
-    def get99(self,im,patch_size=49,stride=24):
-        im_padded = np.zeros((im.shape[0]+patch_size-1,im.shape[1]+patch_size-1,im.shape[2]))
-        im_padded[:,:,0] = np.pad(im[:,:,0], ((patch_size//2, patch_size//2),
-                            (patch_size//2, patch_size//2)), 'constant', constant_values = [0,0])
-        im_padded[:,:,1] = np.pad(im[:,:,1], ((patch_size//2, patch_size//2),
-                            (patch_size//2, patch_size//2)), 'constant', constant_values = [0,0])
-        im_padded[:,:,2] = np.pad(im[:,:,2], ((patch_size//2, patch_size//2),
-                            (patch_size//2, patch_size//2)), 'constant', constant_values = [0,0])
-        output = np.zeros((16,16,2))
-        for i in range(16):
-            for j in range(16):
-                x = np.expand_dims(im_padded[i*stride:i*stride+patch_size,j*stride:j*stride+patch_size,:], axis=0)
-                output[i,j,:] = self.new_model.predict(x,verbose=0)
-        
-        return output
-        
-    def apply_shift_stitch(self,filepath_train='/work/kstandvoss/sas_train_mat.hdf5',filepath_test='/work/kstandvoss/sas_test_mat.hdf5'):
-
-        print('create new train hdf5 file')
-        file = h5py.File(filepath_train, "w")
-        no_chunks = 2
-        dt = h5py.special_dtype(vlen=bytes)
-
-        #3777
-        output = file.create_dataset("probas", (3777, self.img_height, self.img_width), chunks=(no_chunks, self.img_height, self.img_width), dtype='f', compression="lzf")
-        ids = file.create_dataset("ids", (3777,1), chunks=(no_chunks,1), dtype=dt)
-        total = 0
-        for idx,img in enumerate(self.train_images):
-            output[total,:,:] = self.shift_and_stitch(img,patch_size = 49,stride = 24)
-            ids[total,:] = self.train_ids[idx]
-            total += 1
-            if idx%300==0:
-                sys.stdout.write(".")
-                sys.stdout.flush()
-            if idx ==0:
-                break
-        file.flush()
-        file.close()
-
-        print('create new test hdf5 file')
-        file = h5py.File(filepath_test, "w")
-        no_chunks = 2
-        dt = h5py.special_dtype(vlen=bytes)
-        
-        #1000
-        output_test = file.create_dataset("probas", (1000, self.img_height, self.img_width), chunks=(no_chunks, self.img_height, self.img_width), dtype='f', compression="lzf")
-        ids_test = file.create_dataset("ids", (1000,1), chunks=(no_chunks,1), dtype=dt)
-        total = 0
-        for idx,img in enumerate(self.test_images):
-            output_test[total,:,:] = self.shift_and_stitch(img,49,49)
-            ids_test[total,:] = self.test_ids[idx]
-            total += 1
-            if idx%100==0:
-                sys.stdout.write(".")
-                sys.stdout.flush()    
-            if idx == 0:
-                break
-        file.flush()
-        file.close()
-
     def load_model(self,model_name='new_model'):
         # load json and create model
         json_file = open(model_name + '.json', 'r')
@@ -178,33 +74,35 @@ class Pre_Network:
         self.new_model.load_weights(model_name + ".h5")
         print("Loaded model from disk")
 
-    def extract_patches(self,imgs, msks,patch_size=101,stride=50):
+    def extract_patches(self,imgs, msks=None, patch_size=101,stride=50):
         with tf.Session() as sess:    
             patch_arr = tf.extract_image_patches(imgs, ksizes=[1, patch_size, patch_size, 1], strides=[1, stride, stride, 1], rates=[1, 1, 1, 1], padding='SAME').eval()
             dims = patch_arr.shape
             patch_arr = np.reshape(patch_arr,(dims[0]*dims[1]*dims[2],patch_size,patch_size,3))
 
         labels = np.zeros((len(patch_arr),1,1,2))    
-        total = 0
-        for idx,p in enumerate(imgs):
-            for i in range(dims[1]):
-                for j in range(dims[2]):
-                    if np.sum(msks[idx,(i-1)*stride:(i+1)*stride,(j-1)*stride:(j+1)*stride]) >= 1000:
-                        labels[total,:,:,1] = 1
-                    else:
-                        labels[total,:,:,0] = 1
-                    total += 1
-        # sys.stdout.write('\n Loaded :)\n')
+        if msks is not None: 
+          total = 0
+          for idx,p in enumerate(imgs):
+              for i in range(dims[1]):
+                  for j in range(dims[2]):
+                      if np.sum(msks[idx,(i-1)*stride:(i+1)*stride,(j-1)*stride:(j+1)*stride]) >= 1000:
+                          labels[total,:,:,1] = 1
+                      else:
+                          labels[total,:,:,0] = 1
+                      total += 1
         return patch_arr, labels    
 
-    def train_model(self,model_name='new_model'):
+    def train_model(self,model_name='new_model', epochs=1):
         #train your model on data
-        batch_size = 2
-        for batch in range(0,len(self.train_images),batch_size):
-            imgs = self.train_images[batch:batch+batch_size]
-            msks = self.masks[batch:batch+batch_size]
-            img_patches, patch_labels = self.extract_patches(imgs, msks)
-            self.new_model.train_on_batch(img_patches, patch_labels)
+        batch_size = 8
+        for epoch in range(epochs):
+          for batch in range(0,len(self.train_images),batch_size):
+              imgs = self.train_images[batch:batch+batch_size]
+              msks = self.masks[batch:batch+batch_size]
+              img_patches, patch_labels = self.extract_patches(imgs, msks)
+              loss = self.new_model.train_on_batch(img_patches, patch_labels)
+              print('epoch {}: loss = {}'.format(epoch,loss))
 
         # serialize model to JSON
         model_json = self.new_model.to_json()
@@ -214,3 +112,11 @@ class Pre_Network:
         self.new_model.save_weights(model_name + ".h5")
         print("Saved model to disk")
 
+    def predict(self,X_test):
+      img_patches,_ = self.extract_patches(X_test)
+      prediction = self.new_model.predict_on_batch(img_patches)
+      pos_patches = img_patches[prediction.squeeze()[...,1] == 1,:,:,:]  
+      if len(pos_patches)== 0:
+        pos_patches = img_patches[prediction.squeeze()[...,1] == 0,:,:,:]
+      
+      return pos_patches
